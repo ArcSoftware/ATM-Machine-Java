c65846cfa7a88f02f6c9cc5d51cefb4bd98404d7
Jake T <jaketepper@gmail.com>
2017-04-04 16:51:04 -0400

    Added account mod options.

--- src/com/company/RootMenu.java
+++ src/com/company/RootMenu.java
@@ -7,7 +7,7 @@ public class RootMenu {
 
     public void startRootMenu() throws Exception {
         System.out.println("What would you like to do?");
-        System.out.println("Main Menu: \n 1. Check Balance \n 2. Withdraw Funds \n 3. Deposit Funds \n 4. Logout of Account");
+        System.out.println("Main Menu: \n 1. Check Balance \n 2. Withdraw Funds \n 3. Deposit Funds \n 4. Logout of Account \n 5. Modify Accounts ");
         String rootEntry = Main.inputScanner.nextLine();
         double currentBalance = Main.currentUser.balance;
 
@@ -62,9 +62,48 @@ public class RootMenu {
         else if (rootEntry.contains("4") || rootEntry.equalsIgnoreCase("log") || rootEntry.equalsIgnoreCase("out")) {
             Main.end = true;
         }
+        else if (rootEntry.contains("5") || rootEntry.equalsIgnoreCase("Modify") || rootEntry.equalsIgnoreCase("accounts")) {
+            System.out.println("You are now in the account menu. "  + "\n What you like to do?");
+            System.out.println(" 1. Add account \n 2. Remove account \n 3. Account Transfer");
+            String modifyEntry = Main.inputScanner.nextLine();
+            if (modifyEntry.equalsIgnoreCase("1") || modifyEntry.equalsIgnoreCase("add") || modifyEntry.equalsIgnoreCase("add accounts")) {
+                System.out.println("Please enter new account name. \n Ben personally gives all new accounts $100.");
+                String newUser = Main.inputScanner.nextLine();
+                if (Main.accounts.containsKey(newUser)) {
+                    System.out.println("I'm sorry, but there is already an account under that name. \n Returning to main menu... \n");
+                } else {
+                    Account add = new Account();
+                    Main.accounts.put(newUser, add);
+                        System.out.println("An account has been created for " + newUser +  " with $100.00 to spend on us. /dance \n");
+                    }
 
-        else {
-            throw new Exception("Invalid input!");
+                }
+            else if (modifyEntry.equalsIgnoreCase("2") || modifyEntry.equalsIgnoreCase("Remove") || modifyEntry.equalsIgnoreCase("remove accounts")) {
+                System.out.println("Please enter the name of the account you wish to delete.");
+                String removeUser = Main.inputScanner.nextLine();
+                if (Main.accounts.containsKey(removeUser)) {
+                    System.out.println("Are you sure you want to delete " + removeUser + "'s account?");
+                    String confirmDelete = Main.inputScanner.nextLine();
+                    if (confirmDelete.contains("Y") || confirmDelete.contains("y") || confirmDelete.equalsIgnoreCase("confirm")) {
+                        Main.accounts.remove(removeUser);
+                    } else {
+                        System.out.println("Returning to main menu.");
+                    }
+
+                } else {
+                    System.out.println("I'm sorry, there is no account under that name to delete.\n Returning to main menu.. \n");
+                }
+            } else if (modifyEntry.equalsIgnoreCase("3") || modifyEntry.equalsIgnoreCase("transfer") || modifyEntry.equalsIgnoreCase("account transfer")) {
+                System.out.println("Please enter the name of the account you transfer funds to.");
+                String transfer = Main.inputScanner.nextLine();
+                if (Main.accounts.containsKey(transfer)) {
+                    System.out.println("How much of your money would you like to transfer to " + transfer + "'s account?");
+                    String transferAmount = Main.inputScanner.nextLine();
+                    Double.valueOf(transferAmount);
+                    //Main.accounts.replace("transfer", currentBalance + transferAmount);
+                }
+            }
+
         }
 
 
